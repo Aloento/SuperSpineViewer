56699b89cca34aa3349d841b1213e8ee98698526
Aloento <11802769+soarteam@users.noreply.github.com>
2020-12-23 18:36:13 +0100

    首次提交

---
+++ src/main/java/com/QYun/SuperSpineViewer/App.java
@@ -0,0 +1,13 @@
+package com.QYun.SuperSpineViewer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
