7c61f01e65918e202425f025ec5bed5d1fee4429
Aloento <11802769+soarteam@users.noreply.github.com>
2021-01-03 15:12:31 +0100

    骨骼初始化

--- src/main/java/com/QYun/Spine/Spine38.java
+++ src/main/java/com/QYun/Spine/Spine38.java
@@ -1,21 +1,52 @@
 package com.QYun.Spine;
 
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.esotericsoftware.spine38.Skeleton;
+import com.esotericsoftware.spine38.SkeletonBinary;
+import com.esotericsoftware.spine38.SkeletonData;
+import com.esotericsoftware.spine38.SkeletonJson;
+
+import java.util.Objects;
 
 public class Spine38 extends SuperSpine {
 
-    FileHandle skelFile;
-    FileHandle atlasFile;
-    boolean isBinary;
+    private static FileHandle skelFile;
+    private static FileHandle atlasFile;
+    private final boolean isBinary;
+    private SkeletonData skeletonData;
+    private Skeleton skeleton;
 
     public Spine38(FileHandle skelFile, FileHandle atlasFile, boolean isBinary) {
-        this.skelFile = skelFile;
-        this.atlasFile = atlasFile;
+        Spine38.skelFile = Objects.requireNonNull(skelFile);
+        Spine38.atlasFile = Objects.requireNonNull(atlasFile);
         this.isBinary = isBinary;
     }
 
-    private void loadSkel() {
+    private boolean loadSkel() {
+        TextureAtlas atlas = new TextureAtlas(atlasFile);
 
+        if (isBinary) {
+            SkeletonBinary binary = new SkeletonBinary(atlas);
+            binary.setScale(scale);
+            skeletonData = binary.readSkeletonData(skelFile);
+        } else {
+            SkeletonJson json = new SkeletonJson(atlas);
+            json.setScale(scale);
+            skeletonData = json.readSkeletonData(skelFile);
+        }
+        if (skeletonData.getBones().size == 0) {
+            System.out.println("骨骼为空");
+            return false;
+        }
+
+        skeleton = new Skeleton(skeletonData);
+        skeleton.updateWorldTransform();
+        skeleton.setToSetupPose();
+        skeleton.updateWorldTransform();
+
+
+        return true;
     }
 
     @Override
--- src/main/java/com/QYun/Spine/SuperSpine.java
+++ src/main/java/com/QYun/Spine/SuperSpine.java
@@ -4,4 +4,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 
 public class SuperSpine extends ApplicationAdapter {
 
+    static float scale;
+
 }
