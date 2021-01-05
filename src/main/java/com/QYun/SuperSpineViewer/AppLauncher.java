9990f5c95f1527f358ea96f29fd05c7d56a0d837
Aloento <11802769+soarteam@users.noreply.github.com>
2021-01-05 18:15:38 +0100

    弃用JVM参数

---
+++ src/main/java/com/QYun/SuperSpineViewer/AppLauncher.java
@@ -0,0 +1,9 @@
+package com.QYun.SuperSpineViewer;
+
+import javafx.application.Application;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
--- src/main/java/com/QYun/SuperSpineViewer/Main.java
+++ src/main/java/com/QYun/SuperSpineViewer/Main.java
@@ -21,10 +21,6 @@ import java.util.Objects;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) {
 
