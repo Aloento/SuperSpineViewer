e75369d08767c5584b2d758d9740ff14e98b391f
Aloento <11802769+soarteam@users.noreply.github.com>
2021-01-18 00:51:30 +0100

    移除3.7

--- src/main/java/com/esotericsoftware/spine37/Constraint.java
+++
@@ -1,5 +0,0 @@
-package com.esotericsoftware.spine37;
-
-public interface Constraint extends Updatable {
-    int getOrder();
-}
--- src/main/java/com/esotericsoftware/spine37/Updatable.java
+++
@@ -1,5 +0,0 @@
-package com.esotericsoftware.spine37;
-
-public interface Updatable {
-    void update();
-}
