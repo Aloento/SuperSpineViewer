40e4c8260cf2fbbc2761c07374a894b57c3aa9f3
Aloento <11802769+soarteam@users.noreply.github.com>
2021-01-18 15:55:07 +0100

    SpineStandard-TransformConstraint

--- src/main/java/com/esotericsoftware/spine35/TransformConstraint.java
+++ src/main/java/com/esotericsoftware/spine35/TransformConstraint.java
@@ -5,12 +5,7 @@ import com.badlogic.gdx.utils.Array;
 
 import static com.esotericsoftware.spine35.utils.TrigUtils.*;
 
-/**
- * Stores the current pose for a transform constraint. A transform constraint adjusts the world transform of the constrained
- * bones to match that of the target bone.
- * <p>
- * See <a href="http://esotericsoftware.com/spine-transform-constraints">Transform constraints</a> in the Spine User Guide.
- */
+
 public class TransformConstraint implements Constraint {
     final TransformConstraintData data;
     final Array<Bone> bones;
@@ -26,20 +21,18 @@ public class TransformConstraint implements Constraint {
         translateMix = data.translateMix;
         scaleMix = data.scaleMix;
         shearMix = data.shearMix;
-        bones = new Array(data.bones.size);
+        bones = new Array<>(data.bones.size);
         for (BoneData boneData : data.bones)
             bones.add(skeleton.findBone(boneData.name));
         target = skeleton.findBone(data.target.name);
     }
 
-    /**
-     * Copy constructor.
-     */
+    
     public TransformConstraint(TransformConstraint constraint, Skeleton skeleton) {
         if (constraint == null) throw new IllegalArgumentException("constraint cannot be null.");
         if (skeleton == null) throw new IllegalArgumentException("skeleton cannot be null.");
         data = constraint.data;
-        bones = new Array(constraint.bones.size);
+        bones = new Array<>(constraint.bones.size);
         for (Bone bone : constraint.bones)
             bones.add(skeleton.bones.get(bone.data.index));
         target = skeleton.bones.get(constraint.target.data.index);
@@ -49,9 +42,7 @@ public class TransformConstraint implements Constraint {
         shearMix = constraint.shearMix;
     }
 
-    /**
-     * Applies the constraint to the constrained bones.
-     */
+    
     public void apply() {
         update();
     }
@@ -126,16 +117,12 @@ public class TransformConstraint implements Constraint {
         return data.order;
     }
 
-    /**
-     * The bones that will be modified by this transform constraint.
-     */
+    
     public Array<Bone> getBones() {
         return bones;
     }
 
-    /**
-     * The target bone whose world transform will be copied to the constrained bones.
-     */
+    
     public Bone getTarget() {
         return target;
     }
@@ -144,9 +131,7 @@ public class TransformConstraint implements Constraint {
         this.target = target;
     }
 
-    /**
-     * A percentage (0-1) that controls the mix between the constrained and unconstrained rotations.
-     */
+    
     public float getRotateMix() {
         return rotateMix;
     }
@@ -155,9 +140,7 @@ public class TransformConstraint implements Constraint {
         this.rotateMix = rotateMix;
     }
 
-    /**
-     * A percentage (0-1) that controls the mix between the constrained and unconstrained translations.
-     */
+    
     public float getTranslateMix() {
         return translateMix;
     }
@@ -166,9 +149,7 @@ public class TransformConstraint implements Constraint {
         this.translateMix = translateMix;
     }
 
-    /**
-     * A percentage (0-1) that controls the mix between the constrained and unconstrained scales.
-     */
+    
     public float getScaleMix() {
         return scaleMix;
     }
@@ -177,9 +158,7 @@ public class TransformConstraint implements Constraint {
         this.scaleMix = scaleMix;
     }
 
-    /**
-     * A percentage (0-1) that controls the mix between the constrained and unconstrained scales.
-     */
+    
     public float getShearMix() {
         return shearMix;
     }
@@ -188,9 +167,7 @@ public class TransformConstraint implements Constraint {
         this.shearMix = shearMix;
     }
 
-    /**
-     * The transform constraint's setup pose data.
-     */
+    
     public TransformConstraintData getData() {
         return data;
     }
--- src/main/java/com/esotericsoftware/spine35/TransformConstraintData.java
+++ src/main/java/com/esotericsoftware/spine35/TransformConstraintData.java
@@ -2,14 +2,10 @@ package com.esotericsoftware.spine35;
 
 import com.badlogic.gdx.utils.Array;
 
-/**
- * Stores the setup pose for a {@link TransformConstraint}.
- * <p>
- * See <a href="http://esotericsoftware.com/spine-transform-constraints">Transform constraints</a> in the Spine User Guide.
- */
+
 public class TransformConstraintData {
     final String name;
-    final Array<BoneData> bones = new Array();
+    final Array<BoneData> bones = new Array<>();
     int order;
     BoneData target;
     float rotateMix, translateMix, scaleMix, shearMix;
@@ -20,16 +16,12 @@ public class TransformConstraintData {
         this.name = name;
     }
 
-    /**
-     * The transform constraint's name, which is unique within the skeleton.
-     */
+    
     public String getName() {
         return name;
     }
 
-    /**
-     * See {@link Constraint#getOrder()}.
-     */
+    
     public int getOrder() {
         return order;
     }
@@ -38,16 +30,12 @@ public class TransformConstraintData {
         this.order = order;
     }
 
-    /**
-     * The bones that will be modified by this transform constraint.
-     */
+    
     public Array<BoneData> getBones() {
         return bones;
     }
 
-    /**
-     * The target bone whose world transform will be copied to the constrained bones.
-     */
+    
     public BoneData getTarget() {
         return target;
     }
@@ -57,9 +45,7 @@ public class TransformConstraintData {
         this.target = target;
     }
 
-    /**
-     * A percentage (0-1) that controls the mix between the constrained and unconstrained rotations.
-     */
+    
     public float getRotateMix() {
         return rotateMix;
     }
@@ -68,9 +54,7 @@ public class TransformConstraintData {
         this.rotateMix = rotateMix;
     }
 
-    /**
-     * A percentage (0-1) that controls the mix between the constrained and unconstrained translations.
-     */
+    
     public float getTranslateMix() {
         return translateMix;
     }
@@ -79,9 +63,7 @@ public class TransformConstraintData {
         this.translateMix = translateMix;
     }
 
-    /**
-     * A percentage (0-1) that controls the mix between the constrained and unconstrained scales.
-     */
+    
     public float getScaleMix() {
         return scaleMix;
     }
@@ -90,9 +72,7 @@ public class TransformConstraintData {
         this.scaleMix = scaleMix;
     }
 
-    /**
-     * A percentage (0-1) that controls the mix between the constrained and unconstrained shears.
-     */
+    
     public float getShearMix() {
         return shearMix;
     }
@@ -101,9 +81,7 @@ public class TransformConstraintData {
         this.shearMix = shearMix;
     }
 
-    /**
-     * An offset added to the constrained bone rotation.
-     */
+    
     public float getOffsetRotation() {
         return offsetRotation;
     }
@@ -112,9 +90,7 @@ public class TransformConstraintData {
         this.offsetRotation = offsetRotation;
     }
 
-    /**
-     * An offset added to the constrained bone X translation.
-     */
+    
     public float getOffsetX() {
         return offsetX;
     }
@@ -123,9 +99,7 @@ public class TransformConstraintData {
         this.offsetX = offsetX;
     }
 
-    /**
-     * An offset added to the constrained bone Y translation.
-     */
+    
     public float getOffsetY() {
         return offsetY;
     }
@@ -134,9 +108,7 @@ public class TransformConstraintData {
         this.offsetY = offsetY;
     }
 
-    /**
-     * An offset added to the constrained bone scaleX.
-     */
+    
     public float getOffsetScaleX() {
         return offsetScaleX;
     }
@@ -145,9 +117,7 @@ public class TransformConstraintData {
         this.offsetScaleX = offsetScaleX;
     }
 
-    /**
-     * An offset added to the constrained bone scaleY.
-     */
+    
     public float getOffsetScaleY() {
         return offsetScaleY;
     }
@@ -156,9 +126,7 @@ public class TransformConstraintData {
         this.offsetScaleY = offsetScaleY;
     }
 
-    /**
-     * An offset added to the constrained bone shearY.
-     */
+    
     public float getOffsetShearY() {
         return offsetShearY;
     }
